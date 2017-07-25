744fc5b657e572877b28228d63ba3b0071f5419b
Benjamin Ades-Aron <badesar1@gmail.com>
2017-07-25 18:02:11 -0400

    added CSF free smoothing in matlab

---
+++ designer/runsmoothing.m
@@ -0,0 +1,15 @@
+function runsmoothing(root,width,DKIroot)
+addpath(genpath(DKIroot));
+
+nii = load_untouch_nii(fullfile(root,'CSFmask.nii')); csf = logical(nii.img);
+nii = load_untouch_nii(fullfile(root,'dwibc.nii')); dwi = double(nii.img);
+kernelsize = [5 5];
+width = str2double(width);
+
+dwism = smoothing(dwi,kernelsize,width,csf);
+
+nii.img = dwism;
+nii.hdr.dime.glmax = max(dwism(:));
+save_untouch_nii(nii,fullfile(root,'dwi_designer.nii'));
+
+end
