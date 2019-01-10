92d01a2e3cbe0ad4c75f22fdc12178f3fd6cee71
Siddhartha Dhiman <sdhiman@buffalo.edu>
2019-01-10 12:10:00 -0500

    Removed unnecessary files
    Stopped tracking of .DS_Store and removed various files created by SPM or other functions used for testing

--- test/ADC.m
+++
@@ -1,6 +0,0 @@
-function [adc] = ADC(dt, dir)
-[D_ind, D_cnt] = createTensorOrder(2);
-ndir  = size(dir, 1);
-T =  D_cnt(ones(ndir, 1), :).*dir(:,D_ind(:, 1)).*dir(:,D_ind(:, 2));
-adc = T * dt;
-end
