87e0dde1d88412ac888e94fd34020fb3a02569fd
Siddhartha Dhiman <sdhiman@buffalo.edu>
2019-04-26 15:40:09 -0400

    Disable JSON output

--- designer/designer.py
+++ designer/designer.py
@@ -513,7 +513,7 @@ call('mrconvert -force -datatype float32le working.mif dwi_designer.nii',shell=T
             )
 
 #  extract important parameters from working.mif
-call('mrinfo -json_keyval dwi_designer.json -export_grad_fsl dwi_designer.bvec dwi_designer.bval working.mif',shell=True
+call('mrinfo -export_grad_fsl dwi_designer.bvec dwi_designer.bval working.mif',shell=True
             )
 run.function(os.remove, 'working.mif')
 
